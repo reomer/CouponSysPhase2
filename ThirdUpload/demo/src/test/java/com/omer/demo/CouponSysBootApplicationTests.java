a0f20c51fb097f3ca896faf5dd6b97fcd3a5e9e1
reomer <54063314+reomer@users.noreply.github.com>
2020-09-16 17:08:31 +0300

    Add files via upload

---
+++ ThirdUpload/demo/src/test/java/com/omer/demo/CouponSysBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omer.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponSysBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
